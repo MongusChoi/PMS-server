2f85c6c1480fed44d434d98b14715d2d6f20a9e3
MongusChoi <mongus.c@gmail.com>
2021-07-21 23:46:57 +0900

    MongoDB connection function Add

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const { isArray } = require('util')
+
+const global = require('./global')
 
 const app = express()
 const server = require('http').Server(app)
@@ -11,4 +15,19 @@ app.get('/', (req, res) => {
 
 server.listen(port, function () {
   console.log('[system] Open | Port : ' + port)
+})
+
+// DB connect
+mongoose.connect(global.MONGO_CONNECTION_STRING, { 
+  useNewUrlParser: true, 
+  autoReconnect: true, 
+  reconnectTries: Number.MAX_VALUE, 
+  reconnectInterval: 1000, 
+  dbName: global.MONGO_DB_NAME
+})
+
+const db = mongoose.connection
+db.on('error', console.error)
+db.once('open', () => {
+    console.log('[system] mongodb connect')
 })
---
+++ global.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+
+const config = {
+  MONGO_CONNECTION_STRING: 'mongodb+srv://mongus:1394218v@cluster0.scinh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  MONGO_DB_NAME: 'PMS_DB'
+}
+
+module.exports = config
