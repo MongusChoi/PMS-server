4f4c29d4e4c07a2bd5f9fcb3054efa498cb18bd2
MongusChoi <mongus.c@gmail.com>
2021-07-28 00:19:27 +0900

    user model 의 Create 함수 생성, 비밀번호 암호화를 위한 crypto 모듈 추가, global.js 에 hashing 에 필요한 sault 값 추가

--- models/users/partials/create.js
+++ models/users/partials/create.js
@@ -1,3 +1,33 @@
 const { connection } = require('mongoose')
 const { ObjectId } = require('mongodb')
 const userColl = connection.collection('users')
+const crypto = require('crypto')
+const { SECRET } = require('../../../global')
+
+module.exports = {
+  Create: async (param = {}) => {
+    const {
+      id,
+      password,
+      name,
+      position,
+      part,
+      email,
+      joinDate,
+      ip
+    } = param
+
+    const encryptPassword = crypto.createHmac('sha1', SECRET).update(password).digest('base64')
+
+    return await userColl.insertOne({
+      id,
+      password: encryptPassword,
+      name,
+      position,
+      part,
+      email,
+      joinDate,
+      ip
+    })
+  }
+}
