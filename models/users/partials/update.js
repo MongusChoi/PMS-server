428da493ba84c73a2aa5399c7e4214844c1eadf0
MongusChoi <mongus.c@gmail.com>
2021-07-28 00:46:30 +0900

    user model의 UpdateItem 생성

--- models/users/partials/update.js
+++ models/users/partials/update.js
@@ -1,3 +1,35 @@
 const { connection } = require('mongoose')
 const { ObjectId } = require('mongodb')
 const userColl = connection.collection('users')
+const crypto = require('crypto')
+const { SECRET } = require('../../../global')
+
+module.exports = {
+  UpdateItem: async (param = {}) => {
+    const {
+      _id,
+      password,
+      name,
+      position,
+      part,
+      email,
+      joinDate,
+      ip
+    } = param
+
+    let updateQuery = { $set: {} }
+
+    if (password) {
+      const encryptPassword = await crypto.createHmac('sha1', SECRET).update(password).digest('base64')
+      updateQuery.$set.password = encryptPassword
+    }
+    if (name) updateQuery.$set.name = name
+    if (position) updateQuery.$set.position = position
+    if (part) updateQuery.$set.part = part
+    if (email) updateQuery.$set.email = email
+    if (joinDate) updateQuery.$set.joinDate = joinDate
+    if (ip) updateQuery.$set.ip = ip
+
+    return await userColl.updateOne({ _id: ObjectId(_id) }, updateQuery)
+  }
+}
