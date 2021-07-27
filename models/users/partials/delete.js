68bc14f05ff6735823f1ab5146ecc2cc08be290b
MongusChoi <mongus.c@gmail.com>
2021-07-28 01:08:28 +0900

    user model의 DeleteItem 생성

--- models/users/partials/delete.js
+++ models/users/partials/delete.js
@@ -1,3 +1,11 @@
 const { connection } = require('mongoose')
 const { ObjectId } = require('mongodb')
 const userColl = connection.collection('users')
+
+module.exports = {
+  DeleteItem: async (param = {}) => {
+    const { _id } = param
+
+    return userColl.deleteOne({ _id: new ObjectId(_id) })
+  }
+}
