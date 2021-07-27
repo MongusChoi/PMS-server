65b80bd59d090038afc3e8e6a20ebcdc831019d1
MongusChoi <mongus.c@gmail.com>
2021-07-28 00:19:27 +0900

    user model 의 Create 함수 생성, 비밀번호 암호화를 위한 crypto 모듈 추가, global.js 에 hashing 에 필요한 sault 값 추가

--- global.js
+++ global.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 
 const config = {
   MONGO_CONNECTION_STRING: 'mongodb+srv://mongus:1394218v@cluster0.scinh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  MONGO_DB_NAME: 'PMS_DB'
+  MONGO_DB_NAME: 'PMS_DB',
+  SECRET: 'sBnjNWbjSsZyUgthphy',
 }
 
 module.exports = config
